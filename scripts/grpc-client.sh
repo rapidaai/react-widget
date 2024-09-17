763d5bfc63ed82962220ddaf8cfd45116dae84f6
Prashant Srivastav <p_srivastav@outlook.com>
2024-09-17 12:01:02 +0800

    adding to debug

--- scripts/grpc-client.sh
+++ scripts/grpc-client.sh
@@ -2,4 +2,6 @@ PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
 PROTOC_GEN_WEB_PATH="./node_modules/.bin/protoc-gen-grpc-web"
 OUT_DIR="./src/app/clients/protos"
 PROTO_DIR="./src/app/clients/protos"
+
 protoc --plugin="protoc-gen-ts=${PROTOC_GEN_TS_PATH}" --js_out=import_style=commonjs,binary:$OUT_DIR --ts_out=service=grpc-web:$OUT_DIR  -I=$PROTO_DIR "${PROTO_DIR}"/*.proto
+ls ${PROTO_DIR}
